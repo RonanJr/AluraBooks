1d99b68e0837dd880fb8bb08d5c8b6294b29bd30
Ronan <ronan.tecnologo@gmail.com>
2022-12-06 13:57:42 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import Logo from './components/logo';
 import './App.css';
+import perfil from './assets/perfil.svg';
+import sacola from './assets/sacola.svg';
+
+const textoOpcao = ['CATEGORIAS', 'ESTANTE', 'FAVORITOS']
+const icons = [perfil,sacola]
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <Logo></Logo>
+        <ul className='Opcoes'>
+          {textoOpcao.map(
+            (texto) => (<li className='Opcao'><p>{texto}</p></li>)
+          )}
+        </ul>
+
+        <ul className='Icones'>
+          {icons.map( 
+            (icons) => (<li className='Icone'><img src={icons} alt=''></img></li>) 
+            )}
+        </ul>
       </header>
     </div>
   );
